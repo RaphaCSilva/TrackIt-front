6f8eaa8f5a1f7252a81504b69cb7e8a06d1e42ce
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-05-28 16:04:05 -0300

    criandohabito

---
+++ src/componentes/habitocriando.js
@@ -0,0 +1,101 @@
+import React from "react";
+import axios from 'axios';
+import styled from 'styled-components';
+import { Navigate, useNavigate, Link } from "react-router-dom";
+
+export default function Habitocriando() {
+
+    function jogapraCima(){
+
+    }
+
+    return(
+        <>
+        <Caixabranca>
+            <input>
+            </input>
+            <Dias>
+                <button> <h1> D </h1> </button>
+                <button> <h1> S </h1> </button>
+                <button> <h1> T </h1> </button>
+                <button> <h1> Q </h1> </button>
+                <button> <h1> Q </h1> </button>
+                <button> <h1> S </h1> </button>
+                <button> <h1> S </h1> </button>
+            </Dias>
+            <Cancelasalva>
+                <h2 onClick={jogapraCima}>
+                    cancelar
+                </h2>
+                <button>
+                    <h3>
+                        Salvar
+                    </h3>
+                </button>
+            </Cancelasalva>
+        </Caixabranca>
+        </>
+    );
+}
+
+const Caixabranca = styled.div`
+  min-width: 340px;
+  height: 180px;
+  background: #FFFFFF;
+  border-radius: 5px;
+  margin-top: 20px;
+  margin-right: 18px;
+  margin-left: 18px;
+
+  input {
+    width: 303px;
+    height: 45px;
+    background: #FFFFFF;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px; 
+    margin-top: 19px;
+    margin-left: 18px;
+  }
+`;
+
+const Dias = styled.div`
+  margin-top: 8px;
+  margin-left: 18px;
+  button {
+    width: 30px;
+    height: 30px;
+    background: #FFFFFF;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;  
+    margin-right: 4px;
+    h1 {
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19.976px;
+        line-height: 25px;
+        color: #DBDBDB;         
+    }
+  }
+`;
+const Cancelasalva = styled.div`
+  display:flex;
+  margin-right: 16px;
+  margin-bottom: 15px;
+  margin-left: auto;
+  margin-top: 29px;
+  h2 {
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15.976px;
+    line-height: 20px;
+    color: #52B6FF;
+  }
+  button {
+    width: 84px;
+    height: 35px;
+    background: #52B6FF;
+    border-radius: 4.63636px;
+  }
+`;
--- src/componentes/habitos.js
+++ src/componentes/habitos.js
@@ -4,17 +4,46 @@ import styled from 'styled-components';
 import { Navigate, useNavigate, Link } from "react-router-dom";
 import Footer from "./footer";
 import Toper from "./toper";
+import Habito from "./habito";
+import Habitocriando from "./habitocriando";
 
 export default function Habitos() {
+    
     let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQzNywiaWF0IjoxNjUzNzMyNTI5fQ.nsn0VJ5Cr1tQgBInn0aeTDJEDfgpb6h8OwTR-Nwmt_4"
     
+    const [clicou, setClicou] = React.useState(false);
+
+    function adicionarhabito(){
+      if(clicou){
+          setClicou(false);
+          console.log(clicou);
+
+      }else{
+          setClicou(true);
+          console.log(clicou);
+      }
+    }
 
     return(
       <>
       <Toper/>
       <Container>
+        <Menusuperior>  
+          <h1>
+            Meus hábitos
+          </h1>
+          <Adicionar onClick={adicionarhabito}>
+            <h2>
+              +
+            </h2>
+          </Adicionar>
+        </Menusuperior>
+        <EspaçoHabitos>
+          {(clicou === true) && <Habitocriando/>}
+          <Habito>
 
-
+          </Habito>
+        </EspaçoHabitos>
       </Container>
       <Footer/>
       </>
@@ -25,4 +54,42 @@ const Container = styled.div`
   width: 100%;
   height: 1000px;
   background: #E5E5E5;
+`;
+const Menusuperior = styled.div`
+  display: flex;
+  
+  h1 {
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126BA5;
+    padding-top: 98px;
+    padding-left: 17px
+  }
+`;
+const Adicionar = styled.div`
+  width: 40px;
+  height: 35px;
+  margin-left: auto;
+  margin-right: 18px;
+  margin-top: 92px;
+  background: #52B6FF;
+  border-radius: 4.63636px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h2 {
+      font-family: 'Lexend Deca';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 26.976px;
+      line-height: 34px;
+      color: #FFFFFF;
+  }
+`;
+const EspaçoHabitos = styled.div`
+
 `;
